e7d9b938d00adc18ee3911aaa321418e5cd80d4a
liujian <liujian@liujiandeMacBook-Pro.local>
2019-06-30 05:27:19 +0800

    增加build文件夹用于存放编译目标文件，新增Makefile，新增断言实现

--- boot/loader.sh
+++
@@ -1,1 +0,0 @@
-nasm -I ./include/ -o loader.bin loader.S && dd if=./loader.bin  of=../../hd60M.img bs=512 count=3 seek=2 conv=notrunc
---
+++ shellFiles/loader.sh
@@ -0,0 +1,1 @@
+nasm -I ../boot/include/ -o loader.bin loader.S && dd if=./loader.bin  of=../../hd60M.img bs=512 count=3 seek=2 conv=notrunc
